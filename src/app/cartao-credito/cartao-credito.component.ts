fc1e3aaf83a4f758c98c7941138a56018fb8e5df
Gabriel Angelo <gabriel.angelo91@gmail.com>
2017-04-03 23:25:54 -0300

    Inseridas as funções de editar e remover itens

--- src/app/cartao-credito/cartao-credito.component.ts
+++ src/app/cartao-credito/cartao-credito.component.ts
@@ -9,18 +9,31 @@ export class CartaoCreditoComponent implements OnInit{
 
   model = new CompraCartao('', '', 0, 0);
   listaCompras: Array<CompraCartao> = [
-    // new CompraCartao('PAYPAL DO BRASIL', 'Fone de Ouvido na Wallmart', 8, 8.16),
-    // new CompraCartao('LIVRARIA CULTURA FORT', 'Livro Persuassão', 1, 52.10),
-    // new CompraCartao('OUTROS *ESTACIONAM.', '', 3.0, 1.0),
+    new CompraCartao('PAYPAL DO BRASIL', 'Fone de Ouvido na Wallmart', 8, 8.16),
+    new CompraCartao('LIVRARIA CULTURA FORT', 'Livro Persuassão', 1, 52.10),
+    new CompraCartao('OUTROS *ESTACIONAM.', '', 1.0, 3.0),
   ];
+
+  isEdit = false;
+  indexOfObject = null;
 
   constructor() { }
 
   ngOnInit(): void { }
 
   onSubmit() {
-    this.listaCompras.push(this.model);
+    // Verificando se estamos inserindo um objeto ou editando um existente
+    if (!this.isEdit) {
+      this.listaCompras.push(this.model);
+      this.model = new CompraCartao('', '', 0, 0);
+    } else {
+      this.listaCompras[this.indexOfObject] = this.model;
+    }
+
+    // Voltando os valores para edição
     this.model = new CompraCartao('', '', 0, 0);
+    this.indexOfObject = null;
+    this.isEdit = false;
   }
 
   valorMes() {
@@ -41,5 +54,16 @@ export class CartaoCreditoComponent implements OnInit{
     }
 
     return soma;
+  }
+
+  editItem(compra: CompraCartao){
+    this.isEdit = true;
+    this.model = compra;
+    this.indexOfObject = this.listaCompras.indexOf(compra);
+  }
+
+  removeItem(compra){
+    let index = this.listaCompras.indexOf(compra);
+    this.listaCompras.splice(index, 1);
   }
 }
